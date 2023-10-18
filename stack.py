7f6eb683cf19368cb2a9663aa6c738bcc2bb590f
Dynamic Dev <business.dynamiccoding@gmail.com>
2023-10-18 14:51:23 +0100

    Update stack.py
    Added code explaination

--- stack.py
+++ stack.py
@@ -1,3 +1,18 @@
+'''
+This code defines a stack class that allows you to perform basic stack operations such as push, pop, peek, and flush. 
+The `__init__` method initializes the stack with a specified maximum size.
+The `is_empty` method checks if the stack is empty by checking the length of the stack list. If it is empty, it prints a message and returns True. Otherwise, it returns False.
+The `is_full` method checks if the stack is full by comparing the length of the stack list to the capacity. If it is full, it prints a message and returns True. Otherwise, it returns False.
+The `flush` method clears the stack by assigning an empty list to the stack attribute. It first checks if the stack is not empty before performing the operation.
+The `peek` method returns the value at the top of the stack without removing it. It checks if the stack is not empty before printing the value.
+The `pop` method removes the value at the top of the stack. It checks if the stack is not empty before removing the value.
+The `push` method adds a new value to the top of the stack. It checks if the stack is not full before adding the value.
+The `view_stack` method displays all the values in the stack, starting from the top. It checks if the stack is empty before iterating over the stack list and printing each value.
+The `__name__ == "__main__"` block is executed when the script is run directly (not imported as a module). It prompts the user to enter the maximum size of the stack and creates an instance of the stack class.
+It then enters a while loop that repeatedly displays the current stack, prompts the user for a choice, and performs the corresponding operation based on the choice. The loop continues until an invalid choice is made.
+'''
+
+
 class stack:
 
     def __init__(self, maxSize):
